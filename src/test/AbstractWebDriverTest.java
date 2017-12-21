d926d9c01e0a3819a22d074a4b8f327bd2dacb2e
VladimirSh89 <33586213+VladimirSh89@users.noreply.github.com>
2017-12-20 21:50:58 -0500

    add files into the project

---
+++ src/test/AbstractWebDriverTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import TestReporter.JUnitTestReporter;
+import org.junit.After;
+import org.junit.Before;
+
+public class AbstractWebDriverTest extends JUnitTestReporter {
+
+    @Before
+    public void abstractSetUp() {
+
+    }
+
+    @After
+    public void abstractShutDown() {
+
+    }
+
+}
---
+++ src/test/CreateAccountServiceTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CreateAccountServiceTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void shutDown(){
+
+    }
+
+    @Test
+    public void shouldBeAbleToCreateAccount() {
+
+    }
+
+}
